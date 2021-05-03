9c7f715e98ed429f2be24c75eba07a7770c2da1b
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-05-03 17:11:04 +0300

    Update tree.cpp

--- lab4a/tree.cpp
+++ lab4a/tree.cpp
@@ -1,12 +1,9 @@
-#define _CRTDBG_MAP_ALLOC
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdlib.h>
-#include <crtdbg.h> 
-
 #include "tree.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include "for_all.h"
 #include <string.h>
+#include <time.h>
 
 Tree* new_tree(Tree* tree) {
     tree = (Tree*)malloc(sizeof(Tree));
@@ -48,8 +45,8 @@ struct Node* new_node(struct Node* node, char* key, struct Info* info) {
 
 
 void add_tree_no_null(struct Node* node, char* key, struct Info* info) {
-    int result = strcmp(key, node->data.key);//ñðàâíåíèå ïî ASCII
-    if (result > 0) {//êëþ÷ áîëüøå óçëà (èäåì âïðàâî)
+    int result = strcmp(key, node->data.key);//Ã±Ã°Ã Ã¢Ã­Ã¥Ã­Ã¨Ã¥ Ã¯Ã® ASCII
+    if (result > 0) {//ÃªÃ«Ã¾Ã· Ã¡Ã®Ã«Ã¼Ã¸Ã¥ Ã³Ã§Ã«Ã  (Ã¨Ã¤Ã¥Ã¬ Ã¢Ã¯Ã°Ã Ã¢Ã®)
         if (node->right == NULL) {
             node->right = new_node(node->right, key, info);
             node->right->parent = node;
@@ -58,7 +55,7 @@ void add_tree_no_null(struct Node* node, char* key, struct Info* info) {
             add_tree_no_null(node->right, key, info);
         }
     }
-    else if (result < 0) {//êëþ÷ ìåíüøå óçëà (èäåì âëåâî)
+    else if (result < 0) {//ÃªÃ«Ã¾Ã· Ã¬Ã¥Ã­Ã¼Ã¸Ã¥ Ã³Ã§Ã«Ã  (Ã¨Ã¤Ã¥Ã¬ Ã¢Ã«Ã¥Ã¢Ã®)
         if (node->left == NULL) {
             node->left = new_node(node->left, key, info);
             node->left->parent = node;
@@ -67,7 +64,7 @@ void add_tree_no_null(struct Node* node, char* key, struct Info* info) {
             add_tree_no_null(node->left, key, info);
         }
     }
-    else {//êëþ÷è ñâîïàäàþò --> äåëàåì ñïèñîê
+    else {//ÃªÃ«Ã¾Ã·Ã¨ Ã±Ã¢Ã®Ã¯Ã Ã¤Ã Ã¾Ã² --> Ã¤Ã¥Ã«Ã Ã¥Ã¬ Ã±Ã¯Ã¨Ã±Ã®Ãª
         add_next_node(node, info);
         free(key);
     }
@@ -90,12 +87,18 @@ void add_next_node_next(struct Info* info, struct Info* add) {
 ///////////////////////////////////////////////////////////////////
 
 void D_Show(Tree* tree) {
-    /*int choose;
-    printf("Do you want to see tree like usual (choose 2) or back (choose 1)\nChoose-->");
+    int choose;
+    printf("Do you want to see all tree like a normal tree (print 1) or back tree(print 2)?\nChoose-->");
     scan_int(&choose);
-    if (choose == 1){*/
-    show_tree(tree->node);
-    printf("\n");
+    if (choose == 1){
+        int k = print_tree(tree->node, " ", 0);
+        if (k == 1){
+            printf("Tree is empty!\n");
+        }
+    } else{
+        show_tree(tree->node);
+        printf("\n");
+    }
 }
 
 void show_tree(struct Node* node) {
@@ -105,14 +108,14 @@ void show_tree(struct Node* node) {
     else {
         show_tree(node->left);
         show_tree(node->right);
-        show_node(node);
+        show_node(node, 0);
     }
 }
 
-void show_node(struct Node* node) {
+void show_node(struct Node* node, int offset) {
     int i = 0;
     printf("%s //", node->data.key);
-    print_info(node->data.info, i);
+    print_info(node->data.info, i, offset);
 }
 
 ///////////////////////////////////////////////////////////////////
@@ -161,7 +164,7 @@ void D_Find(Tree* tree) {
         if (info == NULL) {
             return;
         }
-        print_info(info, i);
+        print_info(info, i, 0);
     }
     else {
         find_different(tree, key);
@@ -186,25 +189,30 @@ struct Node* find_info_node(struct Node* node, char* key) {
         return NULL;
     }
     else {
-        int result = strcmp(key, node->data.key);//ñðàâíåíèå ïî ASCII
-        if (result > 0) {//êëþ÷ áîëüøå óçëà (èäåì âïðàâî)
+        int result = strcmp(key, node->data.key);//Ã±Ã°Ã Ã¢Ã­Ã¥Ã­Ã¨Ã¥ Ã¯Ã® ASCII
+        if (result > 0) {//ÃªÃ«Ã¾Ã· Ã¡Ã®Ã«Ã¼Ã¸Ã¥ Ã³Ã§Ã«Ã  (Ã¨Ã¤Ã¥Ã¬ Ã¢Ã¯Ã°Ã Ã¢Ã®)
             return find_info_node(node->right, key);
         }
-        else if (result < 0) {//êëþ÷ ìåíüøå óçëà (èäåì âëåâî)
+        else if (result < 0) {//ÃªÃ«Ã¾Ã· Ã¬Ã¥Ã­Ã¼Ã¸Ã¥ Ã³Ã§Ã«Ã  (Ã¨Ã¤Ã¥Ã¬ Ã¢Ã«Ã¥Ã¢Ã®)
             return find_info_node(node->left, key);
         }
         return node;
     }
 }
 
-void print_info(struct Info* info, int i) {
+void print_info(struct Info* info, int i, int offset) {
     if (info == NULL) {
         return;
     }
     else {
         i++;
         printf("%d//%d//%s//release-->%d\n", info->first, info->second, info->word, i);
-        print_info(info->next, i);
+        if (info->next != NULL) {
+            for (int k = 0; k < offset; k++) {
+                printf("\t");
+            }
+        }
+        print_info(info->next, i, offset);
     }
 }
 
@@ -222,18 +230,21 @@ void D_Delete(Tree* tree) {
 void del_node(Tree* tree, char* key) {
     struct Node* node = NULL;
     node = tree->node;
-    node = find_info_node(node, key);//íàõîäèì óçåë ïî êëþ÷ó
+    node = find_info_node(node, key);//Ã­Ã ÃµÃ®Ã¤Ã¨Ã¬ Ã³Ã§Ã¥Ã« Ã¯Ã® ÃªÃ«Ã¾Ã·Ã³
+    if (node == NULL){
+        return;
+    }
     if (node->data.info->next == NULL) {
-        node = del_node_1(tree, node);//ëèñòüÿ
-        if (node == NULL) {//ó÷ïåøíî óäàëèëè ëèñòîê (ïîñëåäíèé ýëåìåíò â âåäêå)
+        node = del_node_1(tree, node);//Ã«Ã¨Ã±Ã²Ã¼Ã¿
+        if (node == NULL) {//Ã³Ã·Ã¯Ã¥Ã¸Ã­Ã® Ã³Ã¤Ã Ã«Ã¨Ã«Ã¨ Ã«Ã¨Ã±Ã²Ã®Ãª (Ã¯Ã®Ã±Ã«Ã¥Ã¤Ã­Ã¨Ã© Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã² Ã¢ Ã¢Ã¥Ã¤ÃªÃ¥)
             return;
         }
-        else if (node->right != NULL && node->left != NULL) {//è ñëåâà è ñïðàâà åñòü ýëåìåíû
+        else if (node->right != NULL && node->left != NULL) {//Ã¨ Ã±Ã«Ã¥Ã¢Ã  Ã¨ Ã±Ã¯Ã°Ã Ã¢Ã  Ã¥Ã±Ã²Ã¼ Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã»
             del_node_2(node);
             return;
         }
         else {
-            del_node_3(tree, node);// åñòü ëèáî ñïðàâà ëèáî ñëåâà
+            del_node_3(tree, node);// Ã¥Ã±Ã²Ã¼ Ã«Ã¨Ã¡Ã® Ã±Ã¯Ã°Ã Ã¢Ã  Ã«Ã¨Ã¡Ã® Ã±Ã«Ã¥Ã¢Ã 
             return;
         }
     }
@@ -244,20 +255,20 @@ void del_node(Tree* tree, char* key) {
 }
 
 Node* del_node_1(Tree* tree, Node* node) {
-    if (node == NULL) {//ìû âîîáùå íå íàøëè òàêîé ýëåìåíò
+    if (node == NULL) {//Ã¬Ã» Ã¢Ã®Ã®Ã¡Ã¹Ã¥ Ã­Ã¥ Ã­Ã Ã¸Ã«Ã¨ Ã²Ã ÃªÃ®Ã© Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²
         return NULL;
     }
-    else if (node->left != NULL || node->right != NULL) {//åñòü õîòÿ áû 1 ïîòîìîê (ìû õîòèì óäàëèòü òîëüêî ïîñëåäíèè ýëåìåíòû)
+    else if (node->left != NULL || node->right != NULL) {//Ã¥Ã±Ã²Ã¼ ÃµÃ®Ã²Ã¿ Ã¡Ã» 1 Ã¯Ã®Ã²Ã®Ã¬Ã®Ãª (Ã¬Ã» ÃµÃ®Ã²Ã¨Ã¬ Ã³Ã¤Ã Ã«Ã¨Ã²Ã¼ Ã²Ã®Ã«Ã¼ÃªÃ® Ã¯Ã®Ã±Ã«Ã¥Ã¤Ã­Ã¨Ã¨ Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²Ã»)
         return node;
     }
     Node* parent = node->parent;
-    if (parent == NULL) {//êñëè äåðåâî ñîñòîèò èç îäíîãî óçëà
+    if (parent == NULL) {//ÃªÃ±Ã«Ã¨ Ã¤Ã¥Ã°Ã¥Ã¢Ã® Ã±Ã®Ã±Ã²Ã®Ã¨Ã² Ã¨Ã§ Ã®Ã¤Ã­Ã®Ã£Ã® Ã³Ã§Ã«Ã 
         tree->node = NULL;
     }
-    else if (parent->left == node) {//åñëè íàø èñêîìûé óçåë ÿâëÿòåñÿ äî÷êîé ñëåâà
+    else if (parent->left == node) {//Ã¥Ã±Ã«Ã¨ Ã­Ã Ã¸ Ã¨Ã±ÃªÃ®Ã¬Ã»Ã© Ã³Ã§Ã¥Ã« Ã¿Ã¢Ã«Ã¿Ã²Ã¥Ã±Ã¿ Ã¤Ã®Ã·ÃªÃ®Ã© Ã±Ã«Ã¥Ã¢Ã 
         parent->left = NULL;
     }
-    else {//ñïðàâà
+    else {//Ã±Ã¯Ã°Ã Ã¢Ã 
         parent->right = NULL;
     }
     free(node->data.info->word);
@@ -267,36 +278,40 @@ Node* del_node_1(Tree* tree, Node* node) {
     return NULL;
 }
 
-void del_node_2(Node* node) {//ñëó÷àé, êîãäà åñòü îáå äî÷êè
-    Node* ptr = node->right;//ñäâèíóëèñü âïðàâî è èäåì äî êîíöà âåäîâî, ÷òîáû âçÿòü ýòîò ýëåìåíò è ïîìåíÿòü íà èñêîìûé(óäîáíî ïîäñòàâëÿòü)
+void del_node_2(Node* node) {
+    Node* ptr = node->right;
+    Node *ptr_prev = (Node*)malloc(sizeof (struct Node));
     struct Node* parent = NULL;
     while (ptr->left != NULL) {
         ptr = ptr->left;
     }
-    node->data.key = node->data.key;//öåëèêîì ïåðåçàïèñûâàåì çíà÷åíèÿ öðàéíå ëåâîãî ëèñòêà
-    node->data.info = node->data.info;
+    ptr_prev->data.key = node->data.key;
+    ptr_prev->data.info = node->data.info;
+    node->data.key = ptr->data.key;
+    node->data.info = ptr->data.info;
     parent = ptr->parent;
-    if (parent->left == ptr) {//îáðûâàåì ñâÿçè
+    if (parent->left == ptr) {
         parent->left = NULL;
     }
     else {
         parent->right = NULL;
     }
-    free(ptr->data.info->word);
-    free(ptr->data.key);
-    free(ptr->data.info);
+    free(ptr_prev->data.info->word);
+    free(ptr_prev->data.key);
+    free(ptr_prev->data.info);
+    free(ptr_prev);
     free(ptr);
 }
 
-void del_node_3(Tree* tree, Node* node) {//ñìûñë- ñîåäåíèòü ðîäèòåëÿ ñ 1 äî÷êîé, ÷òîáû óäàëèòü ïðîìåæóòî÷íûé ýëåìåíò
-    Node* parent = node->parent, * ptr = NULL;
-    if (node->right != NULL) {//åñòü åäèíòñâåííûé ïîòîìê ñïðàâà
+void del_node_3(Tree* tree, Node* node) {//Ã±Ã¬Ã»Ã±Ã«- Ã±Ã®Ã¥Ã¤Ã¥Ã­Ã¨Ã²Ã¼ Ã°Ã®Ã¤Ã¨Ã²Ã¥Ã«Ã¿ Ã± 1 Ã¤Ã®Ã·ÃªÃ®Ã©, Ã·Ã²Ã®Ã¡Ã» Ã³Ã¤Ã Ã«Ã¨Ã²Ã¼ Ã¯Ã°Ã®Ã¬Ã¥Ã¦Ã³Ã²Ã®Ã·Ã­Ã»Ã© Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²
+    Node* parent = node->parent, *ptr = NULL;
+    if (node->right != NULL) {//Ã¥Ã±Ã²Ã¼ Ã¥Ã¤Ã¨Ã­Ã²Ã±Ã¢Ã¥Ã­Ã­Ã»Ã© Ã¯Ã®Ã²Ã®Ã¬Ãª Ã±Ã¯Ã°Ã Ã¢Ã 
         ptr = node->right;
     }
     else {
         ptr = node->left;
     }
-    if (parent == NULL) {//òîëüêî 1 ýëåìåíò
+    if (parent == NULL) {//Ã²Ã®Ã«Ã¼ÃªÃ® 1 Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²
         tree->node = ptr;
     }
     else if (parent->left == node) {
@@ -331,12 +346,14 @@ void del_node_old(struct Node* node) {
 /////////////////////////////////////
 
 void find_different(Tree* tree, char* key) {
-    struct Node* node = NULL;
+    struct Node* node = NULL, *max = NULL, *min = NULL;
     node = tree->node;
     node = find_info_node(node, key);
-    node = go_to_right(node);
+    max = go_to_right(tree->node);
+    min = go_to_left(tree->node);
+    node = comparison(max, min, node);
     printf("%s//", node->data.key);
-    print_info(node->data.info, 0);
+    print_info(node->data.info, 0, 0);
 }
 
 Node* go_to_right(Node* node) {
@@ -349,30 +366,113 @@ Node* go_to_right(Node* node) {
 }
 
 
-////////////////////////////
-
-/*void show_tree_like(struct Node* node, Tree* tree){
-    Node *time = go_to_left(tree->node);
-    int n = time->n;
-    show_tree_like_tree(node, tree, n);
-}
-
-struct Node* go_to_left(Node* node){
+Node *go_to_left(Node* node){
     if (node->left != NULL){
         node = go_to_left(node->left);
-        node->n++;
     } else{
         return node;
     }
 }
 
-void show_tree_like_tree(struct Node* node, Tree* tree, int n){
-    if (node == NULL){
-        printf("");
+Node *comparison(struct Node* max, Node *min, Node *node){
+    int result_max, result_min;
+    result_max = comparison_back(node->data.key, max->data.key);
+    result_min = comparison_back(min->data.key, node->data.key);
+    if (result_max > result_min){
+        return max;
+    } else if (result_min > result_max){
+        return min;
     } else{
-        show_tree_like_tree(node->left, tree, n);
-        show_node(node);
-        show_tree_like_tree(node->right, tree, n);
+        return node;
     }
 }
-*/
+
+int comparison_back(char *key1, char *key2){
+    int i;
+    if (strcmp(key2, key1) == 0){
+        return 0;//ÐºÐ»ÑŽÑ‡Ð¸ Ñ€Ð°Ð²Ð½Ñ‹ -- Ð²Ð¾Ð·Ð²Ñ€Ð°Ñ‰Ð°ÐµÐ¼ Ñ€Ð°Ð·Ð½Ð¸Ñ†Ñƒ 0
+    } else{
+        for (i = 0; i < strlen(key1); i++){
+            if (key1[i] != key2[i]){
+                return ((int)key2[i] - (int)key1[i]);//Ð½Ð°ÑˆÐ»Ð¸ Ð½Ðµ ÑÐ¾Ð²Ð¿Ð°Ð´Ð°ÑŽÑ‰Ð¸Ð¹ - Ð²Ð¾Ð·Ð²Ñ€Ð°Ñ‰Ð°ÐµÐ¼ Ñ€Ð°Ð·Ð½Ð¸Ñ†Ñƒ
+            }
+        }
+        return ((int)key2[i+1]);//Ð¸Ð½Ð°Ñ‡Ðµ- Ð²Ð¾Ð·Ð²Ñ€Ð°Ñ‰Ð°ÐµÐ¼ Ð¿ÐµÑ€Ð²Ñ‹Ð¹ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚, Ð¿Ñ€Ð¸ ÑƒÑÐ»Ð¾Ð²Ð¸Ð¸ Ñ‡Ñ‚Ð¾ ÐºÐ»ÑŽÑ‡Ð¸ Ñ€Ð°Ð·Ð½Ñ‹Ðµ Ð¿Ñ€Ð¸ ÑÑ‚Ð¾ Ð´Ð»Ð¸Ð½Ð° ÐºÐ»ÑŽÑ‡Ð° 1 Ð¼ÐµÐ½ÑŒÑˆÐµ Ð´Ð»Ð¸Ð½Ñ‹ ÐºÐ»ÑŽÑ‡Ð° 2
+    }
+}
+
+
+////////////////////////////
+
+void D_Timing(){
+    Tree *tree_timing = NULL;
+    tree_timing = new_tree(tree_timing);
+    char *key = NULL;
+    Info *info = NULL;
+    int N;
+    printf("Enter count of repeating\nEnter-->");
+    scan_int(&N);
+    char **key_mass = NULL;
+    key_mass = (char **)calloc(N*10, sizeof (char ));
+    printf("Start testing...\n");
+    clock_t begin_add = clock();
+    for (int i = 0; i < N; i++){
+        key = key_random(key);
+        key_mass[i] = key;
+        info = info_rand(info);
+        add_tree(tree_timing, key, info);
+    }
+    clock_t end_add = clock();
+    clock_t begin_find = clock();
+    for (int j = 0; j < N; j++){
+        find_info(tree_timing, key_mass[j]);
+    }
+    clock_t end_find = clock();
+    double time_spent_add = (double )(end_add - begin_add)/CLOCKS_PER_SEC;
+    double time_spent_find = (double )(end_find - begin_find)/CLOCKS_PER_SEC;
+    printf("Testing complete, result of adding-->%f//result of finding-->%f", time_spent_add, time_spent_find);
+    delete_tree(tree_timing);
+    free(key_mass);
+}
+
+char *key_random(char *key){
+    key = (char *)calloc(10, sizeof (char));
+    int i;
+    for (i = 0; i < 10-1; i++){
+        key[i] = 'a'+rand()%26;
+    }
+    key[i] = '\0';
+    return key;
+}
+
+Info *info_rand(struct Info* info){
+    info = (struct Info*)malloc(sizeof (struct Info));
+    info->first = 0;
+    info->second = 0;
+    info->next = NULL;
+    info->word = (char *)malloc(sizeof (char ));
+    *info->word = 'a';
+    return info;
+}
+
+//////////////////////////////////////////////
+
+int print_tree(struct Node* parent, char *key, int offset) {
+    Node *ptr = parent;
+    if (ptr == NULL) {
+        return 1;
+    }
+    if (strcmp(ptr->data.key, key) > 0) {
+        print_tree(ptr->left, key, offset + 1);
+        for (int i = 0; i < offset; i++) {
+            printf("\t");
+        }
+        if (ptr != NULL){
+            show_node(ptr, offset);
+        }
+        print_tree(ptr->right, key, offset + 1);
+    } else {
+        print_tree(ptr->right, key, offset);
+    }
+    return 0;
+}
